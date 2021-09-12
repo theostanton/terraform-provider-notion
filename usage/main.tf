cfd45dc2f1c60e0e0f020fd90512027484234cca
Theo <theo@theo.dev>
2021-09-12 11:28:20 +0200

    Add relation database property

--- usage/main.tf
+++ usage/main.tf
@@ -17,7 +17,7 @@ resource "notion_database" "some_database" {
 }
 
 resource "notion_database" "some_other_database" {
-  title = "Some database"
+  title = "Some other database"
   parent = var.parent_page_id
   title_column_title = "Name"
 }
@@ -26,12 +26,11 @@ resource "notion_database_property_relation" "to_some_other" {
   database = notion_database.some_database.id
   name = "Relation to some other"
   database_id = notion_database.some_other_database.id
-  depends_on = [
-    notion_database.some_other_database,
-    notion_database.some_other_database]
 }
 
+/*
 resource "notion_database_property_select" "severity" {
+  count = 0
   database = notion_database.some_database.id
   name = "Severity title"
   options = {
@@ -40,67 +39,76 @@ resource "notion_database_property_select" "severity" {
   }
 }
 
-//resource "notion_database_property_number" "count" {
-//  database = notion_database.some_database.id
-//  name = "Count"
-//  format = "percent"
-//}
-//
-//resource "notion_database_property_rich_text" "summary" {
-//  database = notion_database.some_database.id
-//  name = "Summary"
-//}
-//
-//resource "notion_database_property_date" "date" {
-//  database = notion_database.some_database.id
-//  name = "Date"
-//}
-//
-//resource "notion_database_property_people" "people" {
-//  database = notion_database.some_database.id
-//  name = "People"
-//}
-//
+resource "notion_database_property_number" "count" {
+  count = 0
+  database = notion_database.some_database.id
+  name = "Count"
+  format = "percent"
+}
 
-//resource "notion_database_property_checkbox" "checkbox" {
-//  database = notion_database.some_database.id
-//  name = "Checkbox"
-//}
-//
-//resource "notion_database_property_url" "url" {
-//  database = notion_database.some_database.id
-//  name = "URL"
-//}
+resource "notion_database_property_rich_text" "summary" {
+  count = 0
+  database = notion_database.some_database.id
+  name = "Summary"
+}
 
-//resource "notion_database_property_email" "email" {
-//  database = notion_database.some_database.id
-//  name = "Email"
-//}
+resource "notion_database_property_date" "date" {
+  count = 0
+  database = notion_database.some_database.id
+  name = "Date"
+}
 
-//resource "notion_database_property_created_time" "created_time" {
-//  database = notion_database.some_database.id
-//  name = "Created time"
-//}
+resource "notion_database_property_people" "people" {
+  count = 0
+  database = notion_database.some_database.id
+  name = "People"
+}
 
-//resource "notion_database_property_created_by" "created_by" {
-//  database = notion_database.some_database.id
-//  name = "Created by"
-//}
 
-//resource "notion_database_property_last_edited_time" "last_edited_time" {
-//  database = notion_database.some_database.id
-//  name = "Last edited time"
-//}
+resource "notion_database_property_checkbox" "checkbox" {
+  count = 0
+  database = notion_database.some_database.id
+  name = "Checkbox"
+}
 
-//resource "notion_database_property_last_edited_by" "last_edited_by" {
-//  database = notion_database.some_database.id
-//  name = "Last edited by"
-//}
-//
-//data "notion_database_entries" "some_database_entries" {
-//  database = notion_database.some_database.id
-//}
+resource "notion_database_property_url" "url" {
+  count = 0
+  database = notion_database.some_database.id
+  name = "URL"
+}
 
-//output "entries" {
-//  value = data.notion_database_entries.some_database_entries.entries
-//}
+resource "notion_database_property_email" "email" {
+  count = 0
+  database = notion_database.some_database.id
+  name = "Email"
+}
+
+resource "notion_database_property_created_time" "created_time" {
+  count = 0
+  database = notion_database.some_database.id
+  name = "Created time"
+}
+
+resource "notion_database_property_created_by" "created_by" {
+  count = 0
+  database = notion_database.some_database.id
+  name = "Created by"
+}
+
+resource "notion_database_property_last_edited_time" "last_edited_time" {
+  count = 0
+  database = notion_database.some_database.id
+  name = "Last edited time"
+}
+
+resource "notion_database_property_last_edited_by" "last_edited_by" {
+  count = 0
+  database = notion_database.some_database.id
+  name = "Last edited by"
+}
+
+data "notion_database_entries" "some_database_entries" {
+  count = 0
+  database = notion_database.some_database.id
+}
+*/
--- usage/outputs.tf
+++ usage/outputs.tf
@@ -1,3 +1,7 @@
 output "database_url" {
   value = notion_database.some_database.url
 }
+
+output "other_database_url" {
+  value = notion_database.some_other_database.url
+}
